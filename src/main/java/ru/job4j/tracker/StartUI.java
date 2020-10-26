6a3f37b5acaf69fb13e043a239a029b7eacccc1c
vnovopashin <novopashinva@gmail.com>
2020-10-26 18:23:17 +0700

    2.1. Реализация класса StartUI [#319005]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -87,14 +87,7 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-       /* Item item = new Item(1, "item");
-        LocalDateTime currentDateTime = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(currentDateTime.format(formatter));
-        System.out.println(item);
-        Tracker tracker = new Tracker();
-        tracker.add(item);
-        System.out.println(tracker.findById(1));*/
+
         Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
         new StartUI().init(scanner, tracker);
